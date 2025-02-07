426fd829703ffd62e227a60a38aefed64d78bc20
Pradeep Joshi <pradeepjoshi6002@gmail.com>
2025-02-07 18:51:55 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import createHttpError, { HttpError } from 'http-errors';
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'this is my app' });
+});
+
+ 
+app.get('/error', (req: Request, res: Response, next: NextFunction) => {
+  const err = createHttpError(401, 'access denied');
+  next(err);
+});
+// app.get('/error', (req: Request, res: Response, next: NextFunction) => {
+//   throw createHttpError(401, 'access denied');
+// });
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    errors: [{ type: err.name, msg: err.message, path: '', location: '' }],
+  });
 });
 
 export default app;
